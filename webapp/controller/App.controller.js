a1feaeb18f33d5d26adc50c7992bd3115ee16728
praveen m <praveengouranga@gmail.com>
2025-04-02 14:51:22 +0000

    model created with binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,24 +4,105 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("project1.controller.App", {
-      onInit() {
+      onInit:function() {
+
+        this.createModel();
       },
 
-      pressMe: function(){
+
+      createModel: function () {
+        var oModel = new sap.ui.model.json.JSONModel()
+        //   step2 set the data inside the model
+        oModel.setData( {
+
+            "empStr": {
+                "empNo": 100,
+                "eName": "krish",
+                "salary": 15000,
+                "currency": "EUR",
+                "smoker": false
+            },
+            "empTab": [
+                {
+                    "empNo": 100,
+                    "eName": "prave",
+                    "salary": 15000,
+                    "currency": "GBP",
+                    "smoker": false
+                },
+                {
+                    "empNo": 100,
+                    "eName": "ram",
+                    "salary": 16000,
+                    "currency": "EUR",
+                    "smoker": false
+                },
+                {
+                    "empNo": 100,
+                    "eName": "kishore",
+                    "salary": 18000,
+                    "currency": "INR",
+                    "smoker": false
+                },
+                {
+                    "empNo": 100,
+                    "eName": "siva",
+                    "salary": 18000,
+                    "currency": "INR",
+                    "smoker": false
+                }
+            ]
+        });
+
+
+
+        sap.ui.getCore().setModel(oModel);
+
+
+
+
+    },
+   
+giveMe: function(){
 
 var val = this.getView().byId("idIn").getValue();
 alert(val);
 
-
-
-
       }
-
-
-
 
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,62 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
-        }
+        },
+        // createModel: function () {
+        //     var oModel = new sap.ui.model.json.JSONModel()
+        //     //   step2 set the data inside the model
+        //     oModel.setData( {
+
+        //         "empStr": {
+        //             "empNo": 100,
+        //             "eName": "krish",
+        //             "salary": 15000,
+        //             "currency": "EUR",
+        //             "smoker": false
+        //         },
+        //         "empTab": [
+        //             {
+        //                 "empNo": 100,
+        //                 "eName": "prave",
+        //                 "salary": 15000,
+        //                 "currency": "GBP",
+        //                 "smoker": false
+        //             },
+        //             {
+        //                 "empNo": 100,
+        //                 "eName": "ram",
+        //                 "salary": 16000,
+        //                 "currency": "EUR",
+        //                 "smoker": false
+        //             },
+        //             {
+        //                 "empNo": 100,
+        //                 "eName": "kishore",
+        //                 "salary": 18000,
+        //                 "currency": "INR",
+        //                 "smoker": false
+        //             },
+        //             {
+        //                 "empNo": 100,
+        //                 "eName": "siva",
+        //                 "salary": 18000,
+        //                 "currency": "INR",
+        //                 "smoker": false
+        //             }
+        //         ]
+        //     });
+
+
+
+        //     sap.ui.getCore().setModel(oModel);
+
+
+
+
+        // }
     });
 });
